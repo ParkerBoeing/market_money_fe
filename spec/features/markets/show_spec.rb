d301204e57f405681172e15c330d6317fcdcf869
Parker Boeing <parker.boeing@gmail.com>
2023-09-10 17:29:46 -0600

    test: User story 2

--- spec/features/markets/show_spec.rb
+++ spec/features/markets/show_spec.rb
@@ -4,26 +4,16 @@ RSpec.describe "Markets Index" do
   describe "discover movies show" do
     it "shows each market with relevant information", :vcr do
       visit market_path("322458")
-
-      within "#322458" do
-        expect(page).to have_content("14&U Farmers' Market")
-        expect(page).to have_content("Washington")
-        expect(page).to have_content("District of Columbia")
-      end
-
-      within "#322474" do
-        expect(page).to have_content("2nd Street Farmers' Market")
-        expect(page).to have_content("Amherst")
-        expect(page).to have_content("Virginia")
-      end
-    end
-
-    it "has a link to affiliated vendor show page", :vcr do
-      visit markets_path
-
-      within "#322458" do
-        click_on "More Info"
-        expect(current_path).to eq(market_path("322458"))
+      
+      expect(page).to have_content("14&U Farmers' Market")
+      expect(page).to have_content("1400 U Street NW")
+      expect(page).to have_content("Washington, District of Columbia 20009")
+      expect(page).to have_content("Affiliated Vendors")
+      
+      within "#55823" do
+        expect(page).to have_content("The Charcuterie Corner")
+        click_on "The Charcuterie Corner"
+        expect(current_path).to eq(vendor_path("55823"))
       end
     end
   end
